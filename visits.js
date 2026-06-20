7e35cc653ec04bb573726763f0261a51eb906758
arabsgate2021 <arabsgate2021@gmail.com>
2026-06-20 14:09:55 +0300

    Remove commented code and storage key definition

--- visits.js
+++ visits.js
@@ -1,10 +1,1 @@
-// نسخ كافة محتويات وسوم <script> هنا
-const STORAGE_KEY = 'asgate_visits_final_v31';
-// ... (قم بنسخ كافة الدوال: loadSavedData, insertNewRow, etc...)
 
-function loadSavedData() {
-    const saved = localStorage.getItem(STORAGE_KEY);
-    if (saved) { document.getElementById('tableBody').innerHTML = ''; JSON.parse(saved).forEach(v => renderRow(v)); }
-    renderActivityLog(); updateStats(); reorderRows();
-}
-// [يتم وضع كامل كود الـ JS هنا]
